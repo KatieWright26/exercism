aa4e8a6ae060b0ebd4eb03cd8a6c907f898e6121
Katie Wright <katie.wrght@gmail.com>
2019-12-23 17:13:35 +0100

    solution for luhn calculation

---
+++ javascript/luhn/luhn.js
@@ -0,0 +1,28 @@
+//
+// This is only a SKELETON file for the 'Luhn' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const valid = cc => {
+  const MIN_LUHN_LENGTH = 2;
+
+  if (cc.trim().length < MIN_LUHN_LENGTH) return false;
+
+  const sanitizedNumber = cc
+  .replace(/\s/g, "")
+  .split("")
+  .map(Number)
+  .reverse();
+
+  const calulateNumber = sanitizedNumber.map((n, i) => {
+      if (i % 2 !== 0) {
+        let doubled = n * 2;
+        n = doubled > 9 ? doubled - 9 : doubled;
+      }
+      return n;
+    }).reduce((num, acc) => acc + num, 0);
+
+  if (calulateNumber % 10 === 0) return true;
+
+  return false;
+};
