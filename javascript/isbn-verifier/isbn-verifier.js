2addc41524e13035af020a473405579bbb4f2271
Katie Wright <katie.wrght@gmail.com>
2019-12-29 12:44:25 +0100

    refactor for loop to be reducer

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -3,21 +3,20 @@ const VALID_ISBN_LENGTH = 10;
 
 export const isValid = (n) => {
   let isbn = n.split("-").join("").split("");
-
   if (isbn.length !== VALID_ISBN_LENGTH) return false;
-  if(!isbn.slice(0, isbn[isbn.length - 1]).map(Number)) return false;
-
   if (isbn[isbn.length - 1] === CHECK_NUMBER) {
     isbn[isbn.length - 1] = VALID_ISBN_LENGTH;
   }
-
   const sanitizedIsbn = isbn.map(Number);
+  if(sanitizedIsbn.some(n => Number.isNaN(n))) return false;
   let count = 10;
-  let toSum = [];
 
-  for(let i = 0; i < sanitizedIsbn.length; i++) {
-    toSum.push(sanitizedIsbn[i] * count);
-    count--;
-  }
-  return toSum.reduce((a,b)=> a + b, 0) % 11 === 0;
+  return sanitizedIsbn.reduce((acc, num) => {
+    if (count > 0) {
+      let calc = num * count;
+      acc += calc;
+      count--;
+    }
+    return acc;
+  }, 0) % 11 === 0;
 };
