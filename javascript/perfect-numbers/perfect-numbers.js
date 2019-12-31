97b1799b2e2fc3075c57a52b92b1869b93d1d1f8
Katie Wright <katie.wrght@gmail.com>
2019-12-31 10:37:11 +0100

    remove unnecessary reduce function

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -2,18 +2,16 @@ export const classify = (n) => {
   if (n <= 0)  throw new Error(
                  'Classification is only possible for natural numbers.'
                );
-  let factors = [];
+  let factors = 0;
 
   for(let f = 1; f < n; f++) {
     if (n % f === 0) {
-      factors.push(f);
+      factors += f;
     }
   }
 
-  let sumFactors = factors.reduce((a, b) => a + b, 0);
-
-  if (sumFactors === n) return 'perfect';
-  if (sumFactors > n) return 'abundant';
+  if (factors === n) return 'perfect';
+  if (factors > n) return 'abundant';
 
   return 'deficient';
 };
