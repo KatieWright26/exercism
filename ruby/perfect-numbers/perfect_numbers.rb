cf571acdae5ffdbda8db4c7aac1f5acd3fd98929
Katie Wright <katie.wrght@gmail.com>
2019-12-31 10:32:38 +0100

    remove excess code from perfect_numbers

--- ruby/perfect-numbers/perfect_numbers.rb
+++ ruby/perfect-numbers/perfect_numbers.rb
@@ -1,18 +1,12 @@
-PERFECT = 'perfect'.freeze
-DEFICIENT = 'deficient'.freeze
-ABUNDANT = 'abundant'.freeze
-
 class PerfectNumber
-  class << self
-    def classify(num)
-      raise RuntimeError if num < 1
+  def self.classify(num)
+    raise RuntimeError if num < 1
 
-      aliquot_sum = 0
-      1.upto(num - 1) { |i| aliquot_sum += i if (num % i).zero? }
-      return ABUNDANT if aliquot_sum > num
-      return DEFICIENT if aliquot_sum < num
+    aliquot_sum = 0
+    1.upto(num - 1) { |i| aliquot_sum += i if (num % i).zero? }
+    return 'abundant' if aliquot_sum > num
+    return 'deficient' if aliquot_sum < num
 
-      PERFECT
-    end
+    'perfect'
   end
 end
