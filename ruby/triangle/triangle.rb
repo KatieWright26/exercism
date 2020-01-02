211824058dd2f8bcfc34c23e7d637a1661a15b16
Katie Wright <katie.wrght@gmail.com>
2020-01-02 11:49:04 +0100

    refactor logic into new method to handle triangle validity

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -5,28 +5,24 @@ class Triangle
   end
 
   def equilateral?
-    return false if sides.min.zero?
+    return true if valid && sides.uniq.count == 1
 
-    sides.uniq.count == 1
-  end
-
-  def scalene?
-    return false if sides.min.zero?
-
-    unique_sides = sides.uniq
-    return false if unique_sides.count < 3
-    return true if (unique_sides.sum - unique_sides.max) > unique_sides.max
+    false
   end
 
   def isosceles?
-    return false if sides.min.zero?
+    return true if valid && sides.uniq.count <= 2
 
-    unique_sides = sides.uniq
-    is_isosceles = unique_sides.count == 2
+    false
+  end
 
-    return false if unique_sides.count > 2
-    return false if is_isosceles && unique_sides.min * 2 < unique_sides.max
+  def scalene?
+    return true if valid && sides.uniq.count == 3
 
-    true
+    false
+  end
+
+  def valid
+    !sides.min.zero? && (sides.uniq.count == 1 || sides.min(2).sum > sides.max)
   end
 end
