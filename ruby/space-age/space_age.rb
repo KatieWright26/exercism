92a356eaf1e990c809404412533cfc5ab0e27887
Katie Wright <katie.wrght@gmail.com>
2020-01-02 08:47:59 +0100

    move logic out of initializer

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -8,20 +8,43 @@ URANUS_YEAR = 84.016846
 NEPTUNE_YEAR = 164.79132
 
 class SpaceAge
-  attr_reader :on_earth, :on_mercury, :on_venus, :on_mars, :on_jupiter,:on_saturn, :on_uranus, :on_neptune
-
   def initialize(seconds)
-    @on_earth = convert_to_earth_years(seconds)
-    @on_mercury = convert_to_earth_years(seconds, MECURY_YEAR)
-    @on_venus = convert_to_earth_years(seconds, VENUS_YEAR)
-    @on_mars = convert_to_earth_years(seconds, MARS_YEAR)
-    @on_jupiter = convert_to_earth_years(seconds, JUPITER_YEAR)
-    @on_saturn = convert_to_earth_years(seconds, SATURN_YEAR)
-    @on_uranus = convert_to_earth_years(seconds, URANUS_YEAR)
-    @on_neptune = convert_to_earth_years(seconds, NEPTUNE_YEAR)
+    @seconds = seconds
   end
 
   def convert_to_earth_years(seconds, planet_year = 1)
     (seconds.to_f / EARTH_YEAR).round(2) / planet_year
+  end
+
+  def on_mercury
+    @on_mercury = convert_to_earth_years(@seconds, MECURY_YEAR)
+  end
+
+  def on_venus
+    @on_venus = convert_to_earth_years(@seconds, VENUS_YEAR)
+  end
+
+  def on_earth
+    @on_earth = convert_to_earth_years(@seconds)
+  end
+
+  def on_mars
+    @on_mars = convert_to_earth_years(@seconds, MARS_YEAR)
+  end
+
+  def on_jupiter
+    @on_jupiter = convert_to_earth_years(@seconds, JUPITER_YEAR)
+  end
+
+  def on_saturn
+    @on_saturn = convert_to_earth_years(@seconds, SATURN_YEAR)
+  end
+
+  def on_uranus
+    @on_uranus = convert_to_earth_years(@seconds, URANUS_YEAR)
+  end
+
+  def on_neptune
+    @on_neptune = convert_to_earth_years(@seconds, NEPTUNE_YEAR)
   end
 end
